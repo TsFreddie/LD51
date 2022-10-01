2aa83fd31a8887ef271a6037a5a0fd78225f8e2e
Freddie <tsfreddiewang@gmail.com>
2022-10-02 00:44:00 +0800

    Replay system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,8 @@ public struct InputState
 {
     public int Move;
     public bool Jump;
+
+    public bool Active => Move != 0 || Jump;
 }
 
 public struct FrameInput
@@ -20,14 +22,27 @@ public struct FrameInput
 
 public class GameManager : MonoBehaviour
 {
-    public FrameInput[] InputRecording;
+    public enum GameState
+    {
+        Waiting,
+        Recording,
+        Playing,
+        Locked,
+    }
+
+    public InputState[] InputRecording;
 
     public static GameManager Instance { get; private set; }
 
-    public InputState LastInput { get; private set; }
+    // public InputState LastInput { get; private set; }
     public InputState CurrentInput { get; private set; }
 
+    public int Frame { get; private set; }
+
+    public Action OnReset;
     public Action OnFixedUpdate;
+
+    public GameState State = GameState.Waiting;
 
     public void Awake()
     {
@@ -39,29 +54,68 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
 
-        var frames = Mathf.FloorToInt(10.0f / Time.fixedDeltaTime);
-        InputRecording = new FrameInput[frames];
+        var frames = Mathf.RoundToInt(10.0f / Time.fixedDeltaTime);
+        InputRecording = new InputState[frames];
         Debug.Log($"Game Manager Initialized with {frames} frames of input");
     }
 
     public void FixedUpdate()
     {
-        LastInput = CurrentInput;
+        // LastInput = CurrentInput;
         CurrentInput = new InputState
         {
             Move = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0),
             Jump = Input.GetKey(KeyCode.Space),
         };
 
+        if (State == GameState.Waiting && CurrentInput.Active)
+        {
+            ResetWorld();
+            State = GameState.Recording;
+        }
+
+        if (State == GameState.Playing && CurrentInput.Active)
+        {
+            State = GameState.Recording;
+        }
+
+        if (State == GameState.Recording)
+        {
+            InputRecording[Frame] = CurrentInput;
+        }
+
+        if (State == GameState.Playing || State == GameState.Recording)
+        {
+            StepFrame();
+            if (Frame == InputRecording.Length)
+            {
+                ResetWorld();
+                State = GameState.Playing;
+            }
+        }
+    }
+
+    private void ResetWorld()
+    {
+        Frame = 0;
+        OnReset?.Invoke();
+    }
+
+    private void StepFrame()
+    {
         OnFixedUpdate?.Invoke();
+        Frame++;
     }
 
     public FrameInput FetchInput()
     {
+        var last = Frame > 0 ? InputRecording[Frame - 1] : default;
+        var current = InputRecording[Frame];
+
         return new FrameInput()
         {
-            LastFrame = LastInput,
-            CurrentFrame = CurrentInput
+            LastFrame = last,
+            CurrentFrame = current,
         };
     }
 }
