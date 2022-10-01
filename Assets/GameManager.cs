7ed1d66e1e3cf925c78dd7862baa7b58e3202cc4
Freddie <tsfreddiewang@gmail.com>
2022-10-01 15:15:03 +0800

    Init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public struct InputState
+{
+    public int Move;
+    public bool Jump;
+}
+
+public struct FrameInput
+{
+    public InputState LastFrame;
+    public InputState CurrentFrame;
+
+    public int Move => CurrentFrame.Move;
+    public bool JumpDown => CurrentFrame.Jump && !LastFrame.Jump;
+    public bool JumpUp => !CurrentFrame.Jump && LastFrame.Jump;
+    public bool Jump => CurrentFrame.Jump;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public FrameInput[] InputRecording;
+    public static GameManager Instance { get; private set; }
+    public InputState CurrentInput { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        var frames = Mathf.FloorToInt(10.0f / Time.fixedDeltaTime);
+        InputRecording = new FrameInput[frames];
+        Debug.Log($"Game Manager Initialized with {frames} frames of input");
+    }
+
+    public void FixedUpdate()
+    {
+        
+    }
+}
